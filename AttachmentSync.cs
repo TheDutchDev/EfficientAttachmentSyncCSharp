bdb1a8fb138dc190645f1d7862088bcaeff44cc3
Niels <nielsvisbeek@outlook.com>
2019-09-15 18:52:13 +0200

    Update AttachmentSync.cs

--- AttachmentSync.cs
+++ AttachmentSync.cs
@@ -1,34 +1,11 @@
-// One could remove this class and move the events to their own events wrapper or whatsoever.
-public class AttachmentSync : Script
+public static class AttachmentSync
 {
-    public AttachmentSync()
-    {
-        Console.WriteLine( "[SYNC] AttachmentSync Initialized!" );
-    }
-
-    [ServerEvent( Event.PlayerConnected )]
-    public void OnPlayerConnect( Client client )
-    {
-        client.SetSharedData( "attachmentsData", "" );
-        client.SetData( "Attachments", new List<uint>( ) );
-    }
-
-    [RemoteEvent( "staticAttachments.Add" )]
-    private void OnStaticAttachmentAdd( Client client, string hash )
-    {
-        client.AddAttachment( Base36Extensions.FromBase36( hash ), false );
-    }
-
-    [RemoteEvent( "staticAttachments.Remove" )]
-    private void OnStaticAttachmentRemove( Client client, string hash )
-    {
-        client.AddAttachment( Base36Extensions.FromBase36( hash ), true );
-    }
-}
-
-// Attachments wrapper
-public static class AttachmentSyncHelpers
-{
+    /// <summary>
+    /// Adds/Removes an attachment for an entity
+    /// </summary>
+    /// <param name="entity">The entity to attach the object to</param>
+    /// <param name="attachment">The attachment, should be in string or long type</param>
+    /// <param name="remove">Pass true to remove the specified attachment, false otherwise.</param>
     public static void AddAttachment( this Entity entity, dynamic attachment, bool remove )
     {
         if( !entity.HasData( "Attachments" ) )
@@ -45,25 +22,31 @@ public static class AttachmentSyncHelpers
 
         if( attachmentHash == 0 )
         {
-            Console.WriteLine( $"Attachment hash couldnt be found for { attachment }" );
+            Console.WriteLine( $"Attachment hash couldn't be found for { attachment }" );
             return;
         }
 
-        if( currentAttachments.IndexOf( attachmentHash ) == -1 )
+        if( currentAttachments.IndexOf( attachmentHash ) == -1 ) // if current attachment hasn't already been added
         {
-            if( !remove )
+            if( !remove ) // if it needs to be added
             {
                 currentAttachments.Add( attachmentHash );
             }
         }
-        else if( remove )
+        else if( remove ) // if it was found and needs to be removed
         {
             currentAttachments.Remove( attachmentHash );
         }
 
-        entity.SetSharedData( "attachmentsData", currentAttachments.Serialize( ) );
+        // send updated data to clientside
+        entity.SetSharedData( "attachmentsData", currentAttachments.Serialize( ) ); 
     }
 
+    /// <summary>
+    /// Serializes a list of attachments
+    /// </summary>
+    /// <param name="attachments">a list of attachments in uint type</param>
+    /// <returns>serialized attachment string</returns>
     public static string Serialize( this List<uint> attachments )
     {
         return string.Join( '|', attachments.Select( a => Base36Extensions.ToBase36( a ) ).ToArray( ) );
